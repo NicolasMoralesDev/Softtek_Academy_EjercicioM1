1746e57965ffc723bed5045b9c313e4996c3a4dc
Nicolas Morales <nicolasmoralesj03@gmail.com>
2023-10-21 18:33:58 -0300

    se termina el algoritmo de suma de dos numeros

---
+++ src/actividadm1/ActividadM1.java
@@ -0,0 +1,32 @@
+
+package actividadm1;
+
+/**
+ *
+ * @author Nico Morales
+ */
+public class ActividadM1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+//        se inizialisan dos variables 
+//        con los nuemeros a sumar
+   
+        int primerNumero = 1;
+        int segundoNumero = 12;
+        
+//        Los valores sumados se guardfan en
+//        una variable
+                
+        int total = primerNumero + segundoNumero;
+        
+        
+//        Se muestra por consola la suma 
+
+        System.out.println("El resultado de la suma de dos numeros es: " + total);
+    }
+    
+}
